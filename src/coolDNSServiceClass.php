065138a383ca7cfbc05d57634fa8651812e0cee4
Yurservice <urserv@gmail.com>
2023-06-09 15:29:55 +0200

    Next step

--- src/coolDNSServiceClass.php
+++ src/coolDNSServiceClass.php
@@ -4,7 +4,7 @@ namespace MiestiechkinApp;
 
 class coolDNSServiceClass
 {
-    public function getDnsRecords($domain_name) {
+    public function getDnsRecords(string $domain_name): array {
         $AllDNSRecords = dns_get_record($domain_name, DNS_ALL);
         return $AllDNSRecords;
     }
